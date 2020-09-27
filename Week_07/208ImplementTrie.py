2b4d02e2a04903e85165173421a2ec250128638a
Si Wang <swang2@woolworths.com.au>
2020-09-27 21:27:26 +1000

    week 7 init

---
+++ Week_07/208ImplementTrie.py
@@ -0,0 +1,41 @@
+class TrieNode:
+
+    # Initialize your data structure here.
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.is_word = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        curr = self.root
+        for c in word:
+            curr = curr.children[c]
+        curr.is_word = True
+
+    def search(self, word):
+        current = self.root
+        for letter in word:
+            current = current.children.get(letter)
+            if current is None:
+                return False
+        return current.is_word
+
+    def startsWith(self, prefix):
+        current = self.root
+        for c in prefix:
+            current = current.children.get(c)
+            if current is None:
+                return False
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
